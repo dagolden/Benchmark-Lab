c2d93d64ce01711729e8d6450cee4b66ddc6b0ed
David Golden <xdg@xdg.me>
2016-01-11 17:09:41 -0500

    Add boilerplate

---
+++ lib/Benchmark/Lab.pm
@@ -0,0 +1,34 @@
+use v5.10;
+use strict;
+use warnings;
+
+package Benchmark::Lab;
+# ABSTRACT: Tools for structured benchmarking and profiling
+
+our $VERSION = '0.001';
+
+1;
+
+=for Pod::Coverage BUILD
+
+=head1 SYNOPSIS
+
+    use Benchmark::Lab;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+=for :list
+* Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et tw=75:
